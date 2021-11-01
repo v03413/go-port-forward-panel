35664c8d14a4ba270ea51dd227303ff2bac456db
zhiqiang.wang <xv12@qq.com>
2021-11-01 16:54:56 +0800

    ➕ 增加依赖 Gin

--- main/main.go
+++ main/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
-func main()  {
-
-	fmt.Println("hello world")
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.Run("127.0.0.1:8080")
 }
